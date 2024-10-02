f7c1dcbdf79b6190a149ce876a665ae196c2f4b0
DevVain <InsaneVain786@gmail.com>
2024-10-02 06:35:12 +0100

    Hook added

--- backend/server.js
+++ backend/server.js
@@ -20,14 +20,14 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-app.use("/api/notifications", notificationRoutes)
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,28 +1,81 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const Posts = () => {
-	const isLoading = false;
+const Posts = ({ feedType }) => {
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "/api/posts/all";
+      case "following":
+        return "/api/posts/following";
+      default:
+        return "/api/posts/all";
+    }
+  };
 
-	return (
-		<>
-			{isLoading && (
-				<div className='flex flex-col justify-center'>
-					<PostSkeleton />
-					<PostSkeleton />
-					<PostSkeleton />
-				</div>
-			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
-				<div>
-					{POSTS.map((post) => (
-						<Post key={post._id} post={post} />
-					))}
-				</div>
-			)}
-		</>
-	);
+  const POST_ENDPOINT = getPostEndpoint();
+
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+    isRefetching,
+    error,
+  } = useQuery({
+    queryKey: ["posts", feedType],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong!");
+        }
+
+        return data;
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        throw new Error(error.message);
+      }
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch]);
+
+  // Check if posts is actually an array
+  const isArrayOfPosts = Array.isArray(posts);
+
+  return (
+    <>
+      {(isLoading || isRefetching) && (
+        <div className="flex flex-col justify-center">
+          <PostSkeleton />
+          <PostSkeleton />
+          <PostSkeleton />
+        </div>
+      )}
+      {!isLoading && !isRefetching && posts?.length === 0 && (
+        <p className="text-center my-4">No posts in this tab. Switch 👻</p>
+      )}
+      {!isLoading && !isRefetching && isArrayOfPosts && posts.length > 0 && (
+        <div>
+          {posts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </div>
+      )}
+      {!isLoading && !isArrayOfPosts && (
+        <p className="text-center my-4">No posts in this tab. Switch 👻</p>
+      )}
+      {error && (
+        <p className="text-center my-4 text-red-500">{error.message}</p>
+      )}
+    </>
+  );
 };
+
 export default Posts;
